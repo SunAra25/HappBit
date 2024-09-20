4b7a979713a35a5e4497f8c586a1cdbe7a8977dc
SunAra25 <tjsdkfk77@naver.com>
2024-09-20 11:30:26 +0900

    [Chore] 폰트 및 컬러 커스텀

---
+++ HappBit/Extension/Color+.swift
@@ -0,0 +1,17 @@
+//
+//  Color+.swift
+//  HappBit
+//
+//  Created by 아라 on 9/20/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let hapRed = Color("Red")
+    static let hapYellow = Color("Yellow")
+    static let hapGreen = Color("Green")
+    static let hapMint = Color("Mint")
+    static let hapBlue = Color("Blue")
+    static let hapPurple = Color("Purple")
+}
---
+++ HappBit/Extension/Font+.swift
@@ -0,0 +1,31 @@
+//
+//  Font+.swift
+//  HappBit
+//
+//  Created by 아라 on 9/20/24.
+//
+
+import SwiftUI
+
+extension Font {
+    public enum FontType: String{
+        case bold = "Bold"
+        case medium = "Medium"
+    }
+    
+    static func Pretendard(_ type: FontType, size: CGFloat) -> Font {
+        return .custom("Pretendard-\(type.rawValue)", size: size)
+    }
+}
+
+extension Font {
+    static let head = Font.Pretendard(.bold, size: 22)
+    static let sub = Font.Pretendard(.bold, size: 20)
+    static let body1B = Font.Pretendard(.bold, size: 16)
+    static let body1M = Font.Pretendard(.medium, size: 16)
+    static let body2B = Font.Pretendard(.bold, size: 14)
+    static let body2M = Font.Pretendard(.medium, size: 14)
+    static let captionB = Font.Pretendard(.bold, size: 13)
+    static let captionM = Font.Pretendard(.medium, size: 13)
+
+}
