04c07307a1889cfd6331b053c77400768eb544c6
SunAra25 <tjsdkfk77@naver.com>
2024-09-21 13:36:40 +0900

    [Feat] Realm 모델 수정

--- HappBit/Sources/Model/Habit.swift
+++ HappBit/Sources/Model/Habit.swift
@@ -1,58 +1,53 @@
+
+//Habit.swift
+//HappBit
 //
-//  Habit.swift
-//  HappBit
-//
-//  Created by 아라 on 9/20/24.
-//
+//Created by 아라 on 9/20/24.
+
 
 import Foundation
 import RealmSwift
 
 final class Habit: Object, ObjectKeyIdentifiable {
-    @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var title: String
-    @Persisted var category: Category
-    @Persisted var habitualization: List<Calendar>
-    @Persisted var cloverCount: Int
-    @Persisted var createdAt: Date
-    @Persisted var endDate: Date?
-    
-    convenience init(title: String,
-                     category: Category,
-                     habitualization: List<Calendar> = List(),
-                     cloverCount: Int = 0,
-                     createdAt: Date = Date(),
-                     endDate: Date? = nil) {
-        self.init()
-        self.title = title
-        self.category = category
-        self.habitualization = habitualization
-        self.cloverCount = cloverCount
-        self.createdAt = createdAt
-        self.endDate = endDate
-    }
+  @Persisted(primaryKey: true) var id: ObjectId
+  @Persisted var title: String
+  @Persisted var category: Category
+  @Persisted var createdAt: Date
+  @Persisted var endDate: Date?
+  
+  convenience init(title: String,
+                   category: Category,
+                   createdAt: Date = Date(),
+                   endDate: Date? = nil) {
+      self.init()
+      self.title = title
+      self.category = category
+      self.createdAt = createdAt
+      self.endDate = endDate
+  }
 }
 
 final class Category: Object, ObjectKeyIdentifiable {
-    @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var title: String
-    @Persisted var color: String
-    
-    convenience init(title: String, color: String) {
-        self.init()
-        self.title = title
-        self.color = color
-    }
+  @Persisted(primaryKey: true) var id: ObjectId
+  @Persisted var title: String
+  @Persisted var color: String
+  
+  convenience init(title: String, color: String) {
+      self.init()
+      self.title = title
+      self.color = color
+  }
 }
 
-final class Calendar: Object, ObjectKeyIdentifiable {
-    @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var month: Int
-    @Persisted var day: Int
-    
-    convenience init(month: Int, day: Int) {
-        self.init()
-        self.month = month
-        self.day = day
-    }
+final class PracticeStatus: Object, ObjectKeyIdentifiable {
+  @Persisted(primaryKey: true) var id: ObjectId
+  @Persisted var habitID: ObjectId
+  @Persisted var practiceDates: List<Date> // 실천 날짜
+  @Persisted var consecutiveDays: Int = 0 // 연속 실천 일수, 클로버 개수
+  @Persisted var currentIndex: Int = 0 // 현재 인덱스 0~3
+  
+  convenience init(habitID: ObjectId) {
+      self.init()
+      self.habitID = habitID
+  }
 }
