efac022502923a4c5625cba8ac18f038ee352e1c
SunAra25 <tjsdkfk77@naver.com>
2024-09-22 11:45:43 +0900

    [Feat] EmptyHabitView(진행중인 습관 없을 때) 구현

--- HappBit/Sources/Presentation/Home/HomeView.swift
+++ HappBit/Sources/Presentation/Home/HomeView.swift
@@ -10,13 +10,52 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                HabitCardView()
-            }
-            .navigationTitle("HappBit")
-            .background(Color.hbSecondary)
-            .shadow(color: .gray.opacity(0.15), radius: 10)
+//            ScrollView {
+//                HabitCardView()
+//            }
+//            .navigationTitle("HappBit")
+//            .background(Color.hbSecondary)
+//            .shadow(color: .gray.opacity(0.15), radius: 10)
+            EmptyHabitView()
+                .navigationTitle("HappBit")
+                .background(Color.hbSecondary)
         }
+    }
+}
+
+struct EmptyHabitView: View {
+    var body: some View {
+        VStack {
+            VStack {
+                Text("현재 진행중인 습관이 없어요")
+                Text("습관을 만들러 가볼까요?")
+            }
+            .padding(.top, 40)
+            
+            NavigationLink {
+                
+            } label: {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.hbThirdary)
+                    .frame(height: 80)
+                    .overlay {
+                        HStack {
+                            Image(systemName: "plus.square")
+                            Text("습관 형성하러 가기")
+                                .padding(.leading, 5)
+                        }
+                        .foregroundStyle(Color.primary)
+                        .font(.body1B)
+                    }
+            }
+            .padding(.top, 20)
+            .padding(.horizontal, 20)
+            
+            Spacer()
+        }
+        .foregroundStyle(Color.gray)
+        .font(.body2M)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
