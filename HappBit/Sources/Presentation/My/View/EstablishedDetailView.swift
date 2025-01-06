0f438a5afa9292281f8ee7ad0bd3122599928238
SunAra25 <tjsdkfk77@naver.com>
2025-01-07 01:07:00 +0900

    [Feat] 자리잡은 습관 상세 뷰 구현

---
+++ HappBit/Sources/Presentation/My/View/EstablishedDetailView.swift
@@ -0,0 +1,101 @@
+//
+//  EstablishDetailView.swift
+//  HappBit
+//
+//  Created by 아라 on 3/24/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct EstablishedDetailView: View {
+    @StateObject private var viewModel = EstablishedDetailViewModel()
+    
+    var body: some View {
+        ScrollView {
+            Text(viewModel.output.title)
+                .font(.head)
+                .padding(.top)
+            
+            HStack {
+                Text(viewModel.output.startDate.toString())
+                    .padding(.vertical, 8)
+                
+                Text("~")
+                
+                Text(viewModel.output.endDate.toString())
+                    .padding(.vertical, 8)
+            }
+            .font(.body2M)
+            .foregroundStyle(.gray)
+            
+            HStack {
+                ForEach(EstablishedCardType.allCases, id: \.self) { type in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.hbQuarterly)
+                            .frame(height: geometry.size.width)
+                    }
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(.horizontal, 4)
+                    .overlay {
+                        switch type {
+                        case .record: recordTextView
+                        case .clover: cloverTextView
+                        }
+                    }
+                }
+            }
+            .padding()
+            
+            CalendarView(date: Date())
+            
+            Spacer()
+        }
+    }
+    
+    var recordTextView: some View {
+        VStack {
+            Text("그동안")
+            
+            HStack(alignment: .bottom) {
+                Text(String(viewModel.output.recordDays))
+                    .font(.head)
+                    .foregroundStyle(Color.hbPrimary)
+                
+                Text("일")
+            }
+            .padding(.bottom, 1)
+            
+            Text("실천했어요")
+        }
+        .font(.body2B)
+        .foregroundStyle(Color.hbPrimary.opacity(0.7))
+    }
+    
+    var cloverTextView: some View {
+        VStack {
+            HStack(alignment: .bottom) {
+                Text(String(viewModel.output.cloverCount))
+                    .font(.head)
+                    .foregroundStyle(Color.hbPrimary)
+                
+                Text("개의")
+            }
+            .padding(.bottom, 1)
+            
+            Text("클로버를 모았어요☘️")
+        }
+        .font(.body2B)
+        .foregroundStyle(Color.hbPrimary.opacity(0.7))
+    }
+}
+
+enum EstablishedCardType: CaseIterable {
+    case record
+    case clover
+}
+
+#Preview {
+    EstablishedDetailView()
+}
---
+++ HappBit/Sources/Presentation/My/ViewModel/EstablishedDetailViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  EstablishedDetailViewModel.swift
+//  HappBit
+//
+//  Created by 아라 on 3/24/25.
+//
+
+import Foundation
+import Combine
+import CoreData
+
+class EstablishedDetailViewModel: ViewModelType {
+    var cancellables = Set<AnyCancellable>()
+    var input = Input()
+    @Published
+    var output = Output()
+    
+    init() {
+        transform()
+    }
+}
+
+// MARK: Input/Output
+extension EstablishedDetailViewModel {
+    struct Input {
+        var viewOnAppear = PassthroughSubject<NSManagedObjectID, Never>()
+        var changeMonthBtnDidTap = PassthroughSubject<Int, Never>()
+    }
+    
+    struct Output {
+        var title: String = ""
+        var startDate: Date = Date()
+        var endDate: Date = Date()
+        var recordDays: Int = 0
+        var cloverCount: Int = 0
+        var records: [Date] = []
+    }
+    
+    func transform() {
+        input
+            .viewOnAppear
+            .sink { [weak self] id in
+                guard let self,
+                      let habit = manager.fetchHabit(id: id),
+                      let records = habit.practiceRecords?.allObjects as? Array<RecordEntity> else { return }
+                
+                output.title = habit.title ?? ""
+                output.startDate = habit.createdAt ?? Date()
+                output.endDate = habit.endDate ?? Date()
+                output.recordDays = records.count
+                let recordsDate = records.map { $0.date ?? Date() }
+                output.records = recordsDate
+                output.cloverCount = manager.calculateCloverCount(recordsDate)
+            }.store(in: &cancellables)
+    }
+}
+
+// MARK: Action
+extension EstablishedDetailViewModel {
+    enum Action {
+        case viewOnAppear(id: NSManagedObjectID)
+        case changeMonthBtnDidTap(Int)
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .viewOnAppear(let id):
+            input.viewOnAppear.send(id)
+        case .changeMonthBtnDidTap(let amount):
+            input.changeMonthBtnDidTap.send(amount)
+        }
+    }
+}
